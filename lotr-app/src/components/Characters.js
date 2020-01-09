c801603b3555bf35caf1d7590d70aac4e38fd151
gabesharpton <gabriel.sharpton@gmail.com>
2020-01-09 14:13:39 -0600

    added pictures

--- lotr-app/src/components/Characters.js
+++ lotr-app/src/components/Characters.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Card, Grid } from 'semantic-ui-react'
+// import { css } from 'glamor';
 
 
 export default function Characters({ data }) {
 
     // const [people, setPeople] = useState([]);
-
+    // const rules = css({
+    //     fontSize: 1
+    //   });
 
     return(
         <div>
         <h1>People</h1>
-        <Grid columns={3}>
+        <Grid columns={4}>
             {data.map((characters, i) => {
                 return (
                     <Grid.Column key={i}>
@@ -18,12 +21,13 @@ export default function Characters({ data }) {
                             <Card.Content>
                                 <Card.Header>{characters.name}</Card.Header>
                                 <Card.Description>
-                                    <strong>Height</strong>
-                                    <p>{characters.height}</p>
-                                    <strong>Mass</strong>
-                                    <p>{characters.mass}</p>
+                                    <strong>Origin</strong>
+                                    <p>{characters.origin.name}</p>
+                                    <strong>Location</strong>
+                                    <p>{characters.location.name}</p>
                                     <strong>Hair Color</strong>
                                     <p>{characters.hair_color}</p>
+                                    <img src={characters.image} style={{height: 150}}></img>
                                 </Card.Description>
                             </Card.Content>
                         </Card>
